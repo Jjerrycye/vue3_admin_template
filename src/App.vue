1a22c26ee50cc5dacd40a45357beaceddfb399f3
Nero <471202797@qq.com>
2023-11-20 14:52:42 +0800

    没有格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+const a = 'asd'
+const fun = ()=>
+{
+  console.log("无格式")
+}
+</script>
+
+<style scoped lang="scss">
+div {
+  background: black;
+}
+</style>
